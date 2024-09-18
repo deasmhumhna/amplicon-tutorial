23f8def625f38d94ad384dc21d653604a8d23dcb
deas-mhumhna <goff.dezmond@gmail.com>
2024-09-17 17:38:38 -0700

    Update install.R

--- install.R
+++ install.R
@@ -1,2 +1,2 @@
 install.packages(c("BiocManager", "decontam", "tidyverse", "phyloseq", "vegan", "DESeq2", "dendextend", "viridis"))
-BiocManager::install("dada2")
+BiocManager::install(c("dada2", "decontam", "phyloseq", "DESeq2"))
